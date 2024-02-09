914ddb7601e5607ee3b9b0f541fe7b77ed7a3ad8
carlosanderson <carlosanderson.net@me.com>
2024-02-09 03:52:10 -0300

    Contato

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -1,4 +1,13 @@
 
 <x-app-layout>
-    contato
+    <div class="container contato">
+        <h1>O que você busca para <br> Sua Empresa?</h1>
+        <subtitle>Nulla mauris justo, porta eu scelerisque ac, lacinia quis sem_acquire</subtitle>
+
+        <form>
+            <input type="text" placeholder="Nome">
+            <input type="text" placeholder="Deixe seu telefone">
+<button class="readmore">Ver Mais</button>
+        </form>
+    </div>
 </x-app-layout>
