d224beb02e786fe9f5562919e6679ac397c63157
carlosanderson <carlosanderson.net@me.com>
2024-02-10 00:56:33 -0300

    filtro completo

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -1,29 +1,42 @@
 <x-app-layout>
+    <TitleCLients>
+        <container class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h1>Clientes</h1>
+                    <subtitle>Instigar a pessoa a conhecer nossos antigos trabalhos.</subtitle>
+                </div>
+                <div class="col-6">
+                    <p>  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+        </container>
+    </TitleCLients>
     <section id="gallery">
         <div class="galleryfilter"> 
             <ul class="filter-menu">
-                <li data-target="all">All</li>
-                <li data-target="men">MEN</li>
-                <li data-target="kid">KID</li>
-                <li data-target="female">FEMALE</li>
+                <li data-target="all">Todos</li>
+                <li data-target="servicos">Serviços</li>
+                <li data-target="area">Área</li>
+                <li data-target="comunicacao">Comunicação no Design</li>
             </ul>
             <ul class="filter-item">
-                <li data-item="men">
+                <li data-item="servicos" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/mr-khushal.jpg">
                 </li>
-                <li data-item="men">
+                <li data-item="servicos" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/khushal.jpg">
                 </li>
-                <li data-item="kid">
+                <li data-item="area" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-baby-boy.jpg">
                 </li>
-                <li data-item="kid">
+                <li data-item="area comunicacao" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-smile-baby-boy.jpg">
                 </li>
-                <li data-item="female">
+                <li data-item="comunicacao" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-smile-girl.jpg">
                 </li>
-                <li data-item="female">
+                <li data-item="comunicacao" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-girl.jpg">
             </li>
             </ul>
