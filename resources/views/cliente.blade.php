b09d6c7448851843b34b80f342016b6e2bbdc4ee
carlosanderson <carlosanderson.net@me.com>
2024-02-10 01:10:31 -0300

    filtro com detalhes dos clientes

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -22,22 +22,46 @@
             </ul>
             <ul class="filter-item">
                 <li data-item="servicos" class="col-4">
-                <img src="https://www.myprograming.com/wp-content/uploads/2021/10/mr-khushal.jpg">
+                    <img src="https://www.myprograming.com/wp-content/uploads/2021/10/mr-khushal.jpg">
+                    <detalhesCliente>
+                        <h4>CLienteX</h4>
+                        <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </detalhesCliente>
                 </li>
                 <li data-item="servicos" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/khushal.jpg">
+                    <detalhesCliente>
+                        <h4>CLienteX</h4>
+                        <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </detalhesCliente>
                 </li>
                 <li data-item="area" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-baby-boy.jpg">
+                    <detalhesCliente>
+                        <h4>CLienteX</h4>
+                        <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </detalhesCliente>
                 </li>
                 <li data-item="area comunicacao" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-smile-baby-boy.jpg">
+                    <detalhesCliente>
+                        <h4>CLienteX</h4>
+                        <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </detalhesCliente>
                 </li>
                 <li data-item="comunicacao" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-smile-girl.jpg">
+                    <detalhesCliente>
+                        <h4>CLienteX</h4>
+                        <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </detalhesCliente>
                 </li>
                 <li data-item="comunicacao" class="col-4">
                 <img src="https://www.myprograming.com/wp-content/uploads/2021/10/cute-girl.jpg">
+                    <detalhesCliente>
+                        <h4>CLienteX</h4>
+                        <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </detalhesCliente>
             </li>
             </ul>
         </div>
